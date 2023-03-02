65831a8602e9a5a2edac6a82f3e28b44a68315ff
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-02 17:22:20 +0700

    :money_with_wings: add cors func

---
+++ config/cor.go
@@ -0,0 +1,18 @@
+package config
+
+import "net/http"
+
+func Cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
--- routes/router.go
+++ routes/router.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"api/config"
 	"fmt"
 	"net/http"
 	"os"
@@ -10,19 +11,10 @@ import (
 
 func Router() {
 	r := mux.NewRouter()
+	r.Use(config.Cors)
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Welcome to the home page!")
-	})
-
-	r.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "List of products")
-	})
-
-	r.HandleFunc("/products/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		productID := vars["id"]
-		fmt.Fprintf(w, "Showing product with ID %v", productID)
+		fmt.Fprint(w, "Welcome to the blank space")
 	})
 
 	http.ListenAndServe(":"+os.Getenv("PORT"), r)
