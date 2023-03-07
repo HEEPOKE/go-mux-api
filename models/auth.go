e9d96a6bfbc3bd29fe02386eb8cab64925e0278e
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-07 11:04:30 +0700

    :money_with_wings: add model auth

---
+++ models/auth.go
@@ -0,0 +1,9 @@
+package models
+
+import "gorm.io/gorm"
+
+type Auth struct {
+	gorm.Model
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
--- models/user.go
+++ models/user.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 	Email    string `json:"email"`
 	Tel      string `json:"tel"`
 	Role     string `json:"role"`
