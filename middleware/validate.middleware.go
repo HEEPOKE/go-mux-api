3c5d042db2488493792e753dd51400412a804520
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-07 14:02:36 +0700

    Update validate.middleware.go

--- middleware/validate.middleware.go
+++ middleware/validate.middleware.go
@@ -11,13 +11,12 @@ import (
 	"github.com/golang-jwt/jwt"
 )
 
-var secret = []byte(os.Getenv("JWT_SECRET"))
-
 func JwtMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		secret := []byte(os.Getenv("JWT_SECRET"))
 		header := r.Header.Get("Authorization")
 		tokenString := strings.Split(header, " ")
-		fmt.Println(tokenString[1])
+
 		if len(tokenString) != 2 {
 			common.RespondWithError(w, http.StatusUnauthorized, "Token Invalid")
 			return
@@ -26,7 +25,6 @@ func JwtMiddleware(next http.Handler) http.Handler {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 			}
-
 			return secret, nil
 		})
 
@@ -42,7 +40,6 @@ func JwtMiddleware(next http.Handler) http.Handler {
 			common.RespondWithError(w, http.StatusUnauthorized, "Token expired")
 			return
 		}
-
 		next.ServeHTTP(w, r)
 	})
 }
